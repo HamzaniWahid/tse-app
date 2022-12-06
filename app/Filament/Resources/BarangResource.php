d806cdcb10912544ac299176d91c911239f875a1
HamzaniWahid <110075981+HamzaniWahid@users.noreply.github.com>
2022-12-06 15:33:08 +0800

    first commit

---
+++ app/Filament/Resources/BarangResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BarangResource\Pages;
+use App\Filament\Resources\BarangResource\RelationManagers;
+use App\Models\Barang;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BarangResource extends Resource
+{
+    protected static ?string $model = Barang::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('jenis')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('kategori')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('ket')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama'),
+                Tables\Columns\TextColumn::make('jenis'),
+                Tables\Columns\TextColumn::make('kategori'),
+                Tables\Columns\TextColumn::make('ket'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBarangs::route('/'),
+            'create' => Pages\CreateBarang::route('/create'),
+            'edit' => Pages\EditBarang::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ app/Filament/Resources/ServiceResource/Pages/Settings.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ServiceResource\Pages;
+
+use App\Filament\Resources\ServiceResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class Settings extends ManageRecords
+{
+    protected static string $resource = ServiceResource::class;
+}
